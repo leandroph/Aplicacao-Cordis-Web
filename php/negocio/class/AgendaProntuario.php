79d1fc756214378b8b332f7796c7e28468cbafbe
leandroph <leandroph1992@hotmail.com>
2019-08-16 18:58:51 -0300

    add classes

---
+++ php/negocio/class/AgendaProntuario.php
@@ -0,0 +1,70 @@
+<?php
+class AgendaProntuario
+{
+    // declaração de propriedade
+    private $id;
+    private $idProntuario;
+    private $idAgendamento;
+	
+	// Geters and Seters
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idProntuario
+     */ 
+    public function getIdProntuario()
+    {
+        return $this->idProntuario;
+    }
+
+    /**
+     * Set the value of idProntuario
+     *
+     * @return  self
+     */ 
+    public function setIdProntuario($idProntuario)
+    {
+        $this->idProntuario = $idProntuario;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idAgendamento
+     */ 
+    public function getIdAgendamento()
+    {
+        return $this->idAgendamento;
+    }
+
+    /**
+     * Set the value of idAgendamento
+     *
+     * @return  self
+     */ 
+    public function setIdAgendamento($idAgendamento)
+    {
+        $this->idAgendamento = $idAgendamento;
+
+        return $this;
+    }
+}
---
+++ php/negocio/class/Agendamento.php
@@ -0,0 +1,158 @@
+<?php
+class Agendamento
+{
+    // declaração de propriedade
+    private $id;
+    private $dataHoraInicio;
+    private $dataHoraFim;
+    private $valor;
+    private $observacao;
+    private $status;
+    private $pago;
+	
+	// Geters and Seters
+	
+
+	
+
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dataHoraInicio
+     */ 
+    public function getDataHoraInicio()
+    {
+        return $this->dataHoraInicio;
+    }
+
+    /**
+     * Set the value of dataHoraInicio
+     *
+     * @return  self
+     */ 
+    public function setDataHoraInicio($dataHoraInicio)
+    {
+        $this->dataHoraInicio = $dataHoraInicio;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dataHoraFim
+     */ 
+    public function getDataHoraFim()
+    {
+        return $this->dataHoraFim;
+    }
+
+    /**
+     * Set the value of dataHoraFim
+     *
+     * @return  self
+     */ 
+    public function setDataHoraFim($dataHoraFim)
+    {
+        $this->dataHoraFim = $dataHoraFim;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of valor
+     */ 
+    public function getValor()
+    {
+        return $this->valor;
+    }
+
+    /**
+     * Set the value of valor
+     *
+     * @return  self
+     */ 
+    public function setValor($valor)
+    {
+        $this->valor = $valor;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of observacao
+     */ 
+    public function getObservacao()
+    {
+        return $this->observacao;
+    }
+
+    /**
+     * Set the value of observacao
+     *
+     * @return  self
+     */ 
+    public function setObservacao($observacao)
+    {
+        $this->observacao = $observacao;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of status
+     */ 
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set the value of status
+     *
+     * @return  self
+     */ 
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of pago
+     */ 
+    public function getPago()
+    {
+        return $this->pago;
+    }
+
+    /**
+     * Set the value of pago
+     *
+     * @return  self
+     */ 
+    public function setPago($pago)
+    {
+        $this->pago = $pago;
+
+        return $this;
+    }
+}
