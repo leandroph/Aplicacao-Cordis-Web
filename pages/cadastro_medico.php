afc945b8a522ac0b4700df325810a0ea4bda3ba0
Daniel Buchholz <danielbuchholz16@gmail.com>
2019-12-04 13:22:52 -0300

    Alterações

--- pages/cadastro_medico.php
+++ pages/cadastro_medico.php
@@ -37,6 +37,8 @@ $preferencia = $preferenciaDAO->getPreferencia($id_usuario_logado);
 $medicoDAO = new MedicoDAO($conexao);
 $medicoListaCadastro = $medicoDAO->getMedicos();
 
+$usuarioLista = $usuarioDAO->getUsuarios();
+
 $enderecoDAO = new EnderecoDAO($conexao);
 
 $paisDAO = new PaisDAO($conexao);
@@ -58,7 +60,9 @@ $pag = $_GET['pag'];
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 2 | Calendar</title>
+    <title>Clinica Cordis</title>
+    <link rel="icon" href="../dist/img/cardiogram.png" type="image/x-icon" />
+    <link rel="shortcut icon" href="../dist/img/cardiogram.png" type="image/x-icon" />
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
@@ -116,8 +120,8 @@ $pag = $_GET['pag'];
                             <div class="box-header">
                                 <div class="btn-group">
                                     <button type="button" class="btn btn-success" data-toggle="modal" data-target="#my_modal_add">Cadastrar Médico</button>
-                                    <!-- <button type="button" class="btn btn-warning btn-flat" data-toggle="modal" data-target="#alterar">Alterar Médico</button>
-                                    <button type="button" class="btn btn-danger" onclick="atualizar()">Excluir Médico</button> -->
+                                    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#my_modal_vinculo">Adicionar Permissão</button>
+                                    <!-- <button type="button" class="btn btn-danger" onclick="atualizar()">Excluir Médico</button> -->
                                 </div>
                             </div>
                             <!-- /.box-header -->
@@ -466,6 +470,179 @@ $pag = $_GET['pag'];
                                 </form>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal" id="my_modal_vinculo">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title text-center"><b>Vincular Médico</b></h4>
+                </div>
+                <div class="modal-body" style="">
+                    <div class="box-body">
+                        <h4><b>Lista de Usuarios</b></h3>
+                            <table id="example2" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 10px;">#</th>
+                                        <th>Nome</th>
+                                        <th>Email</th>
+                                        <th>CPF</th>
+                                        <th style="width: 120px;"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+
+                                    <?php
+                                    $count = 0;
+
+                                    if ($usuarioLista != null) {
+
+                                        foreach ($usuarioLista as $lista) {
+                                            $count = $count +1;
+                                            $pessoaLista = $pessoaDAO->getPessoa($lista->getId());
+                                            $medicoLista = $medicoDAO->getMedico($lista->getId());
+
+                                            // echo $medicoLista->getId_Usuario();
+                                            
+                                            if ($lista->getPerm_Medico()==0) {
+                                                echo "<tr>";
+                                                echo "<td style='display: table-cell; vertical-align:middle; height:100%;'>" . $count . "</td>";
+                                                echo "<td style='display: table-cell; vertical-align:middle; height:100%;'>" . $pessoaLista->getNome() . " " . $pessoaLista->getSobrenome() . "</td>";
+                                                echo "<td style='display: table-cell; vertical-align:middle; height:100%;'>" . $pessoaLista->getEmail() . "</td>";
+                                                // echo "<td style='display: table-cell; vertical-align:middle; height:100%;'>" . $medicoLista->getCRM() . "</td>";
+                                                // echo "<td><a class='btn btn-info' role='button' data-toggle='modal' data-target='#my_modal' data-id='" . $pessoaLista->getId_Usuario() . "' 
+                                                // data-nome='" . $pessoaLista->getNome() . "' data-sobrenome='" . $pessoaLista->getSobrenome() . "' data-cpf='" . $pessoaLista->getCPF() . "'
+                                                // data-rg='" . $pessoaLista->getRG() . "' data-sexo='" . $pessoaLista->getSexo() . "' data-nascimento='" . $pessoaLista->getNascimento() . "'
+                                                // data-email='" . $pessoaLista->getEmail() . "' data-logradouro='" . $pessoaEndereco->getLogradouro() . "' data-bairro='" . $pessoaEndereco->getBairro() . "'
+                                                // data-cep='" . $pessoaEndereco->getCEP() . "' data-complemento='" . $pessoaEndereco->getComplemento() . "' data-numero='" . $pessoaEndereco->getNumero() . "'
+                                                // data-cidade='" . $pessoaEndereco->getId_cidade() . "' data-crm='" . $medicoLista->getCRM() . "' data-especialidade='" . $medicoLista->getEspecialidade() . "'
+                                                // data-agenda='" . $medicoLista->getCorAgenda() . "' data-estado='" . $idEstado->getId() . "' data-pais='" . $idPais->getId() . "'
+                                                // >Editar</a>
+                                                //     <a class='btn btn-danger' data-toggle='modal' data-target='#my_modal_del'" . $pessoaLista->getId_Usuario() . "' data-id='" . $pessoaLista->getId_Usuario() . "' data-nome='" . $pessoaLista->getNome() . ' ' . $pessoaLista->getSobrenome() . "'>Deletar</a><br/></td>";
+                                                echo "</tr>";
+                                            }
+                                        }
+                                    } else {
+                                        echo "<p>Nenhum usuário cadastrado</p>";
+                                    }
+                                    ?>
+                                    <!-- <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td> -->
+
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th style="width: 10px;">#</th>
+                                        <th>Nome</th>
+                                        <th>Email</th>
+                                        <th>CPF</th>
+                                        <th></th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                            <h4><b>Lista de Médicos Desativados</b></h3>
+                                <table id="example3" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th style="width: 10px;">#</th>
+                                            <th>Nome</th>
+                                            <th>Email</th>
+                                            <th>CPF</th>
+                                            <th>CRM</th>
+                                            <th style="width: 120px;"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+
+                                        <?php
+                                        $count = 0;
+
+                                        if ($medicoListaCadastro != null) {
+                                            foreach ($medicoListaCadastro as $medicoLista) {
+                                                $pessoaLista = $pessoaDAO->getPessoa($medicoLista->getId_Usuario());
+                                                $pessoaEndereco = $enderecoDAO->getEndereco($pessoaLista->getId_Endereco());
+                                                $count = $count + 1;
+                                                $idCidade = $cidadeDAO->getCidade($pessoaEndereco->getId_cidade());
+                                                $idEstado = $estadoDAO->getEstado($idCidade->getId_Estado());
+                                                $idPais = $paisDAO->getPais($idEstado->getId_Pais());
+                                                if ($medicoLista->getAtivo() == 1) {
+                                                    echo "<tr>";
+                                                    echo "<td style='display: table-cell; vertical-align:middle; height:100%;'>" . $count . "</td>";
+                                                    echo "<td style='display: table-cell; vertical-align:middle; height:100%;'>" . $pessoaLista->getNome() . " " . $pessoaLista->getSobrenome() . "</td>";
+                                                    echo "<td style='display: table-cell; vertical-align:middle; height:100%;'>" . $pessoaLista->getEmail() . "</td>";
+                                                    echo "<td style='display: table-cell; vertical-align:middle; height:100%;'>" . $medicoLista->getCRM() . "</td>";
+                                                    echo "<td><a class='btn btn-info' role='button' data-toggle='modal' data-target='#my_modal' data-id='" . $pessoaLista->getId_Usuario() . "' 
+                                                data-nome='" . $pessoaLista->getNome() . "' data-sobrenome='" . $pessoaLista->getSobrenome() . "' data-cpf='" . $pessoaLista->getCPF() . "'
+                                                data-rg='" . $pessoaLista->getRG() . "' data-sexo='" . $pessoaLista->getSexo() . "' data-nascimento='" . $pessoaLista->getNascimento() . "'
+                                                data-email='" . $pessoaLista->getEmail() . "' data-logradouro='" . $pessoaEndereco->getLogradouro() . "' data-bairro='" . $pessoaEndereco->getBairro() . "'
+                                                data-cep='" . $pessoaEndereco->getCEP() . "' data-complemento='" . $pessoaEndereco->getComplemento() . "' data-numero='" . $pessoaEndereco->getNumero() . "'
+                                                data-cidade='" . $pessoaEndereco->getId_cidade() . "' data-crm='" . $medicoLista->getCRM() . "' data-especialidade='" . $medicoLista->getEspecialidade() . "'
+                                                data-agenda='" . $medicoLista->getCorAgenda() . "' data-estado='" . $idEstado->getId() . "' data-pais='" . $idPais->getId() . "'>Editar</a>
+                                                    <a class='btn btn-danger' data-toggle='modal' data-target='#my_modal_del'" . $pessoaLista->getId_Usuario() . "' data-id='" . $pessoaLista->getId_Usuario() . "' data-nome='" . $pessoaLista->getNome() . ' ' . $pessoaLista->getSobrenome() . "'>Deletar</a><br/></td>";
+                                                    echo "</tr>";
+                                                }
+                                            }
+                                        } else {
+                                            echo "<p>Nenhum usuário cadastrado</p>";
+                                        }
+                                        ?>
+                                        <!-- <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td> -->
+
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th style="width: 10px;">#</th>
+                                            <th>Nome</th>
+                                            <th>Email</th>
+                                            <th>CPF</th>
+                                            <th>CRM</th>
+                                            <th></th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                                <div class="box box-warning">
+                                    <div class="box-header with-border ">
+                                        <h3 class="box-title text-center">Tem certeza que deseja escluir este registro?</h3>
+                                        <br>
+                                        <form action="excluir.php" method="get">
+                                            <div class="row">
+                                                <div class="col-xs-6">
+                                                    <div class="input-group">
+                                                        <span class="input-group-addon"><b>ID</b></span>
+                                                        <input readonly type="text" class="form-control" name="id" placeholder="">
+                                                    </div>
+                                                </div>
+                                                <div class="col-xs-6">
+                                                    <div class="input-group">
+                                                        <span class="input-group-addon"><b>Nome</b></span>
+                                                        <input readonly type="text" class="form-control" name="nome" placeholder="">
+                                                        <input readonly type="hidden" class="form-control" name="tipo" value="medico">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <br>
+                                            <div class="text-right">
+                                                <button class="btn btn-success" type="submit">Sim</button>
+                                                <button type="button" class="btn btn-danger" data-dismiss="modal">Não</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
                     </div>
                 </div>
             </div>
@@ -1030,7 +1207,9 @@ $pag = $_GET['pag'];
 
     <script>
         $(function() {
-            $('#example1').DataTable()
+            $('#example1').DataTable();
+            $('#example2').DataTable();
+            $('#example3').DataTable();
         })
     </script>
 
